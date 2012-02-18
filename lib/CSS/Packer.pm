032a4766c44d48bd25ad5e55b0fea5e8a7e8911b
Merten Falk <merten.falk@xing.com>
2012-02-19 00:03:30 +0100

    Version 1.002001
    - Use gnutar to build distribution.

--- lib/CSS/Packer.pm
+++ lib/CSS/Packer.pm
@@ -6,7 +6,7 @@ use strict;
 use Carp;
 use Regexp::RegGrp;
 
-our $VERSION            = '1.002';
+our $VERSION            = '1.002001';
 
 our @COMPRESS           = ( 'minify', 'pretty' );
 our $DEFAULT_COMPRESS   = 'pretty';
@@ -367,7 +367,7 @@ CSS::Packer - Another CSS minifier
 
 =head1 VERSION
 
-Version 1.002
+Version 1.002001
 
 =head1 DESCRIPTION
 
