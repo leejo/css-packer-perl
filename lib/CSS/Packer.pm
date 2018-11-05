c38446eedb0acca940be76d743c182b3a492494a
zdm <zdm@softvisio.net>
2018-11-05 16:21:01 +0200

    indent option added # no filter

--- lib/CSS/Packer.pm
+++ lib/CSS/Packer.pm
@@ -124,6 +124,7 @@ our $reggrp_declaration;
 our $reggrp_mediarules;
 our $reggrp_content_value;
 our $global_compress;
+our $indent = '';
 
 sub init {
     my $class   = shift;
@@ -210,7 +211,7 @@ sub init {
 
                 return '' if ( not $key or $value eq '' );
 
-                return $key . ':' . $value . ';' . ( $compress eq 'pretty' ? "\n" : '' );
+                return $indent . $key . ':' . $value . ';' . ( $compress eq 'pretty' ? "\n" : '' );
             }
         }
     ];
@@ -339,6 +340,8 @@ sub minify {
         foreach my $field ( 'compress', 'copyright' ) {
             $self->$field( $opts->{$field} ) if ( defined( $opts->{$field} ) );
         }
+
+        $indet = $opts->{indent} && ( !$opts->{compress} || $opts->{compress} eq 'pretty' ) ? ' ' x $opts->{indent} : '';
     }
 
 	# (re)initialize variables used in the closures
